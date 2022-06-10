29bbd10a2907166061847615607abf57191bc286
tanjid111 <tanjidhossain@gmail.com>
2022-06-09 21:30:38 -0700

    skills

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
 import LogoComponents from '../subComponents/LogoComponents';
@@ -8,9 +8,7 @@ import { Projects } from '../data/ProjectsData'
 import ProjectComponent from './ProjectComponent';
 import AnchorComponent from '../subComponents/Anchor';
 
-const ProjectsPage = () => {
-
-    const MainContainer = styled.div`
+const MainContainer = styled.div`
     background-image: url(${img});
     background-size: cover;
     background-repeat: no-repeat;
@@ -20,25 +18,34 @@ const ProjectsPage = () => {
 
     `
 
-    const Container = styled.div`
+const Container = styled.div`
     background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
     width: 100%;
    
     position: relative;
     padding-bottom: 5rem;    
     `
-    const Center = styled.div`
+const Center = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     padding-top: 10rem;
     `
 
-    const Grid = styled.div`
+const Grid = styled.div`
     display: grid;
     grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
     grid-gap: calc(1rem + 2vw);
     `
+
+const ProjectsPage = () => {
+
+    const [numbers, setNumbers] = useState(0);
+
+    useEffect(() => {
+        let num = (window.innerHeight - 70) / 30;
+        setNumbers(parseInt(num));
+    }, [])
 
     return (
         <MainContainer>
@@ -46,7 +53,7 @@ const ProjectsPage = () => {
                 <LogoComponents />
                 <PowerButton />
                 <SocialIcons />
-                <AnchorComponent />
+                <AnchorComponent numbers={numbers} />
                 <Center>
                     <Grid>
                         {
