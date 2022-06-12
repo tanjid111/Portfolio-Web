ca0376821cc418187de9b28b43061559d3a5920b
tanjid111 <tanjidhossain@gmail.com>
2022-06-11 23:27:27 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion"
-import { Switch, Route, useLocation } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 import AboutPage from "./components/AboutPage"
@@ -13,6 +13,7 @@ import GlobalStyle from "./globalStyles"
 import SoundBar from "./subComponents/SoundBar"
 import ContactPage from "./components/ContactPage"
 import { createContext, useState } from "react"
+
 
 
 export const ProjectContext = createContext();
@@ -31,14 +32,14 @@ function App() {
         <SoundBar />
 
         <AnimatePresence exitBeforeEnter>
-          <Switch location={location} key={location.pathname}>
-            <Route exact path='/' component={Main}></Route>
-            <Route exact path='/about' component={AboutPage}></Route>
-            <Route exact path='/projects' component={ProjectsPage}></Route>
-            <Route exact path='/blogs' component={Blogs}></Route>
-            <Route exact path='/skills' component={MySkillsPage}></Route>
-            <Route exact path='/contact' component={ContactPage}></Route>
-          </Switch>
+          <Routes location={location} key={location.pathname}>
+            <Route path='/' element={<Main />}></Route>
+            <Route path='/about' element={<AboutPage />}></Route>
+            <Route path='/projects' element={<ProjectsPage />}></Route>
+            <Route path='/blogs' element={<Blogs />}></Route>
+            <Route path='/skills' element={<MySkillsPage />}></Route>
+            <Route path='/contact' element={<ContactPage />}></Route>
+          </Routes>
         </AnimatePresence>
       </ThemeProvider>
     </ProjectContext.Provider>
